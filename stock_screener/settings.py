20d8e23910fd144a36feee423f07e688599d90f8
Arib Farooqui <aribbabar100@gmail.com>
2024-01-15 15:37:23 -0500

    Update ALLOWED_HOSTS in settings.py

--- stock_screener/settings.py
+++ stock_screener/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = config["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["35.209.67.131", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["35.209.67.131", "localhost",
+                 "127.0.0.1", "stockscreener.chalkys.net"]
 
 
 # Application definition
